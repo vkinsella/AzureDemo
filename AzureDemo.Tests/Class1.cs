5984e78c04a2692cb57796a316c5d5957606f2ae
vkinsella <vkinsella@A-JHWH453>
2024-01-25 12:04:21 +0000

    Add project files.

---
+++ AzureDemo.Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit;
+using NUnit.Framework;
+using AzureDemo;
+
+namespace AzureDemo.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public static void Test1()
+        {
+            
+            int ans = Program.Add(3, 5);
+            Assert.That(ans, Is.EqualTo(8));
+
+
+
+        }
+
+    }
+    
+}
---
+++ AzureDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureDemo
+{
+    public static class Program
+    {
+        
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
